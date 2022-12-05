4d442612c79e8b6c8206ef3c8fdd1df44ef2bd5b
Dan Miller <dtmpost@gmail.com>
2022-12-05 13:50:57 +0000

    adding solutions for day 5

---
+++ 5-supply-stacks/puzzle_1.py
@@ -0,0 +1,31 @@
+import re
+from collections import deque, namedtuple
+from io import StringIO
+
+import pandas as pd
+
+
+CrateMovement = namedtuple("CrateMovement", "number_of_crates from_stack to_stack")
+stacks = []
+crate_movements = []
+stack_tops = []
+
+with open("input.txt") as f:
+    match = re.search(r"(?P<crates>(.|\n)*)(\n{2})(?P<instructions>(.|\n)*)", f.read())
+    crates = match.group("crates")
+    instructions = match.group("instructions")
+
+df = pd.read_fwf(StringIO(crates), header=None, skipfooter=1)
+
+for column in df.columns:
+    stacks.append(deque([item.translate(str.maketrans("", "", "[]")) for item in df[column] if not(pd.isnull(item))]))
+
+for instruction in instructions.split("\n"):
+    match = re.search(r"move (\b[0-9]+\b) from (\b[0-9]+\b) to (\b[0-9]+\b)", instruction)
+    crate_movements.append((CrateMovement(int(match.group(1)), int(match.group(2)), int(match.group(3)))))
+
+for crate_movement in crate_movements:
+    for crate in range(crate_movement.number_of_crates):
+        stacks[crate_movement.to_stack-1].appendleft(stacks[crate_movement.from_stack-1].popleft())
+
+print("puzzle 1:", "".join([stack[0] for stack in stacks]))
